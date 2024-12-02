7d31a9ab8028bd4729702cc54c78e368133e8b66
a119b <66J20883@utas.edu.om>
2024-12-02 15:24:46 +0400

    successfully

---
+++ client/src/Components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h3>Badriya - 66J20883</h3>
+    </div>
+  );
+};
+
+export default About;
---
+++ client/src/Exampledata.js
@@ -0,0 +1,22 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Badriya",
+    email: "66j20883@utas.edu.om",
+    password: "1234bb",
+  },
+
+  {
+    id: 2,
+    name: "Mohammed",
+    email: "66j123@utas.edu.om",
+    password: "1234mm",
+  },
+
+  {
+    id: 3,
+    name: "Ahmed",
+    email: "66s444@utas.edu.om",
+    password: "1234aa",
+  },
+];
